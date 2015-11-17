b57b267287100264aa4ac0e1550be59d88bb9f98
JulioDVega <julio.delavega96@gmail.com>
2015-11-17 14:30:36 -0600

    timmer game updated

---
+++ js/timergame.js
@@ -0,0 +1,14 @@
+var counter = 0;
+function countup() {
+  counter = counter + 1;
+  }
+
+
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("Wait a Second you clicked " +counter+ " Hero's!");
+     
+     }, 20000);
+
+}
